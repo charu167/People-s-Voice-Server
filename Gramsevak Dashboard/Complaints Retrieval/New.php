d23ef312b32352994c9b12c0d2f7223299cb956c
dhanashreemunot <dhanashreemunot106@gmail.com>
2021-10-19 16:15:45 +0530

    description added as a column in data table

--- Gramsevak Dashboard/Complaints Retrieval/New.php
+++ Gramsevak Dashboard/Complaints Retrieval/New.php
@@ -5,7 +5,7 @@ include '../../db.php';
 
 $k = getallheaders();
 $region = $k['region'];
-$new_complaints = mysqli_query($conn, "SELECT C_ID, u_name, c_location, forGS, forAdmin FROM tbl_Complaint WHERE c_region = '$region' AND forGS = '1'");
+$new_complaints = mysqli_query($conn, "SELECT C_ID, u_name, c_location,c_description, forGS, forAdmin FROM tbl_Complaint WHERE c_region = '$region' AND forGS = '1'");
 $rows = array();
 while ($r = mysqli_fetch_assoc($new_complaints)) {
     $rows[] = $r;
