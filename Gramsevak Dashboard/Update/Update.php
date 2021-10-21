9b70d6124015c37e9313e4c033ef76b37026024e
dhanashreemunot <dhanashreemunot106@gmail.com>
2021-10-21 23:38:12 +0530

    Update.php code changes

--- Gramsevak Dashboard/Update/Update.php
+++ Gramsevak Dashboard/Update/Update.php
@@ -1,15 +1,47 @@
 <?php
 session_start();
 include '../../db.php';
-$putdata = fopen("php://input", 'r');
-$str = stream_get_contents($putdata);
-// fclose($putdata);
-
-// $sql = "UPDATE tbl_complaint SET forAdmin = '1' WHERE C_ID = $str";
 
 
-// if (mysqli_query($conn, $sql)) {
-//     echo $str;
-// }
+$k = getallheaders();
+$region = $k['region'];
+$qry = mysqli_query($conn,"select * from tbl_gramsevak where region='$region'"); 
+$data = mysqli_fetch_assoc($qry);
 
-echo $str ;
+$e=$data['email'];
+$n=$data['name'];
+$a=$data['address'];
+$p=$data['phone'];
+$pa=$data['password'];
+
+$email = empty($_POST['email'])? $e:$_POST['email'];
+
+$name = empty($_POST['name'])? $n:$_POST['name'];
+$address = empty($_POST['address'])? $a:$_POST['address'];
+$phone ="";
+if($_POST['phone']==NULL){
+    $phone = $p;   
+}
+else{
+    $phone =$_POST['phone'];
+}
+
+
+
+$password = empty($_POST['password'])? $pa:md5($_POST['password']);
+
+
+
+
+
+$edit = mysqli_query($conn,"update tbl_gramsevak set name='$name', email='$email', password='$password', address='$address', phone='$phone' where region='$region'");
+
+if($edit)
+{ 
+    echo 123;
+}
+else
+{
+    echo mysqli_error();
+}    	
+
