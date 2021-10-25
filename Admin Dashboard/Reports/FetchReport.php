b14352725631317df7704a0e22eee4e4955fde83
Charudatta Potdar <charupot08@gmail.com>
2021-10-25 13:08:46 +0530

    minor changes

--- Admin Dashboard/Reports/FetchReport.php
+++ Admin Dashboard/Reports/FetchReport.php
@@ -3,7 +3,7 @@ session_start();
 include "../../db.php";
 
 $sql = "SELECT 
-        c.u_name, c.u_address, c.u_phone,c.c_type, c.forAdmin, c.c_region, DATE_FORMAT(c.c_date, '%M %d %Y') as date , g.name as g 
+        c.u_name, c.u_address, c.u_phone, c.c_type, c.forAdmin, c.c_region, DATE_FORMAT(c.c_date, '%M %d %Y') as date , g.name as g 
         from tbl_complaint c 
         INNER JOIN 
         tbl_gramsevak g
